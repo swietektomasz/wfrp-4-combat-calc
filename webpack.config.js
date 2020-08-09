e60bdfe8023586360ec93bd594cf1edb72c8fb5d
swietektomasz <swietek.tomasz@protonmail.com>
2020-08-09 10:31:25 +0200

    Webpack config, required packages

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    compress: true,
+    quiet: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/assets/favicon.png",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      shared: path.resolve(__dirname, "./src/components/shared"),
+    },
+  },
+};
